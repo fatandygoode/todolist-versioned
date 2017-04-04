1f11f1eb6314a0b6c8bfe313c878e12973e604dd
fatandygoode <johnmadden86@gmail.com>
2017-04-04 10:35:11 +0100

    introduced delete todo support

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -1,12 +1,30 @@
 package controllers;
 
+import models.Todo;
 import play.Logger;
 import play.mvc.Controller;
+import java.util.List;
 
-public class Dashboard extends Controller
-{
+public class Dashboard extends Controller {
+
   public static void index() {
     Logger.info("Rendering Dashboard");
-    render ("dashboard.html");
+    List<Todo> todolist = Todo.findAll();
+    render("dashboard.html", todolist);
+  }
+
+  public static void addTodo(String title) {
+    Todo todo = new Todo(title);
+    todo.save();
+    Logger.info("Addint Todo" + title);
+    redirect("/dashboard");
+  }
+
+  public static void deleteTodo(Long id)
+  {
+    Todo todo = Todo.findById(id);
+    todo.delete();
+    Logger.info("Deleting " + todo.title);
+    redirect("/dashboard");
   }
 }
