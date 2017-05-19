19c6ac3beb9306fec3ff380c4d11fef36bef6acb
John Madden <johnmadden86@gmail.com>
2017-05-19 02:52:32 +0100

    done, but not fully complete

--- app/models/Member.java
+++ app/models/Member.java
@@ -46,6 +46,50 @@ public class Member extends Person {
         return assessments;
     }
 
+    /**
+     * Calculate a member's body mass index, weight divided by height squared
+     * @return  Member's BMI (kg m ^-2) truncated to two decimal places
+     */
+    public double calculateBMI(){
+        return toTwoDecimalPlaces(getCurrentWeight() / (getHeight()/100 * getHeight()/100));
+    }
+    /**
+     * helper method to truncate numbers to two decimal places
+     * @return number to two decimal places
+     */
+    private static double toTwoDecimalPlaces(double num){
+        return (int) (num * 100 ) / 100.0;
+    }
+
+    public String isIdealBodyWeight() {
+        double inchHeight = convertHeightMetresToInches(getHeight());
+        if (inchHeight > 60) {
+            inchHeight = inchHeight - 60;
+        } else {
+            inchHeight = 0;
+        }
+        double idealBodyWeight = 2.3 * inchHeight;
+        if (getGender().equals("Male")) {
+            idealBodyWeight = idealBodyWeight + 50;
+        } else {
+            idealBodyWeight = idealBodyWeight + 45.5;
+        }
+        if (idealBodyWeight <= getCurrentWeight() + 2 && idealBodyWeight >= getCurrentWeight() - 2) {
+            return "Ideal Body weight";
+        } else {
+            return "Not ideal body weight";
+        }
+    }
+
+    /**
+     * Converts a member's height from metres to inches, 1m = 39.37 inches.
+     * @return  Member's height in inches truncated to two decimal places
+     */
+    public double convertHeightMetresToInches(double height) {
+        return toTwoDecimalPlaces(height * 39.37/100);
+    }
+
+
     public Assessment getAssessment(int index) {
         return getAssessments().get(index);
     }
@@ -63,6 +107,40 @@ public class Member extends Person {
     }
 
     public Assessment latestAssessment() {
-        return assessments.get(numberOfAssessments()-1);
+        return getAssessments().get(0);
+    }
+
+
+    /**
+     * Determines the BMI catergory to which the member belongs.
+     * The categories are determined as follows:
+     * less than 15 "VERY SEVERELY UNDERWEIGHT"
+     * between 15 (inclusive) and 16 (exclusive) "SEVERELY UNDERWEIGHT"
+     * between 16 (inclusive) and 18.5 (exclusive) "UNDERWEIGHT"
+     * between 18.5 (inclusive) and 25 (exclusive) "NORMAL"
+     * between 25 (inclusive) and 30 (exclusive) "OVERWEIGHT"
+     * between 30 (inclusive) and 35 (exclusive) "MODERATELY OBESE"
+     * between 35 (inclusive) and 40 (exclusive) "SEVERELY OBESE"
+     * greater than 40 "VERY SEVERELY OBESE"
+     * @return  Member's BMI category
+     */
+    public String determineBMICategory(double bmiValue) {
+        if (bmiValue < 15) {
+            return "Very severely underweight";
+        } else if (bmiValue >= 15 && bmiValue < 16) {
+            return "Severely underweight";
+        } else if (bmiValue >= 16 && bmiValue < 18.5) {
+            return "Underweight";
+        } else if (bmiValue >= 18.5 && bmiValue < 25) {
+            return "Normal";
+        } else if (bmiValue >= 25 && bmiValue < 30) {
+            return "Overweight";
+        } else if (bmiValue >= 30 && bmiValue < 35) {
+            return "Moderately Obese";
+        } else if (bmiValue >= 35 && bmiValue < 40) {
+            return "Severely Obese";
+        } else {
+            return "Very severely obese";
+        }
     }
 }
